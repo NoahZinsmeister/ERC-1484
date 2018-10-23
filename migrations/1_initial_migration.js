1fa307f899de98c135528c5f535deaa7213aa254
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-10-23 15:38:02 -0400

    only use migrations to link libraries

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,20 +1,10 @@
 const AddressSet = artifacts.require('./AddressSet/AddressSet.sol')
+
 const AddressSetTest = artifacts.require('./AddressSet/AddressSetTest.sol')
-
 const IdentityRegistry = artifacts.require('./IdentityRegistry.sol')
-
-const Provider = artifacts.require('./testing/Provider.sol')
-const Resolver = artifacts.require('./testing/Resolver.sol')
 
 module.exports = async function (deployer) {
   deployer.deploy(AddressSet)
   deployer.link(AddressSet, AddressSetTest)
-  deployer.deploy(AddressSetTest)
-
   deployer.link(AddressSet, IdentityRegistry)
-  await deployer.deploy(IdentityRegistry)
-
-  const identityRegistry = await IdentityRegistry.deployed()
-  deployer.deploy(Provider, identityRegistry.address)
-  deployer.deploy(Resolver, identityRegistry.address)
 }

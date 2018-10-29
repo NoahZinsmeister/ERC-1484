9c06e4e4fecdaa160e63ab67d545c1690f27e24f
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-10-29 13:02:02 -0400

    add travis + coveralls integrations

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 8545,
       network_id: '*',
+      websockets: true
+    },
+    coverage: {
+      host: 'localhost',
+      port: 8555,
+      network_id: '*',
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
       websockets: true
     }
   },

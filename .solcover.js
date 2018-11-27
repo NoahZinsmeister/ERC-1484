65034191ae1ccf06cf8555e679790a14c11edca3
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-11-27 16:06:43 -0500

    ignore interfaces for coverage purposes

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-  skipFiles: ['_testing/', 'examples/Resolvers/ERC725/ClaimHolder.sol', 'examples/Resolvers/ERC725/KeyHolder.sol'],
+  skipFiles: ['_testing/', '_interfaces', 'examples/Resolvers/ERC725/ClaimHolder.sol', 'examples/Resolvers/ERC725/KeyHolder.sol'],
   compileCommand: 'npm run build',
   testCommand: 'npm run test-cover',
   norpc: true,
